6d694269fe19e9e62b8624537c6bfadc8493e756
FabianReyes2002 <reyesfabian2@outlook.com>
2025-11-28 11:40:21 +0100

    Update website content

--- src/HomeSection.jsx
+++ src/HomeSection.jsx
@@ -6,7 +6,7 @@ import CarouselComponent from "./CarouselComponent";
 export default function HomeSection({ projects }) {
   return (
     <div className="container">
-      <Typography variant="h4" className="section-title">Fabian Reyes</Typography>
+      <Typography variant="h4" className="section-title">Fabian Reyes!</Typography>
       <Box sx={{ display: "grid", placeItems: "center" }}>
         <CarouselComponent projects={projects} />
       </Box>
